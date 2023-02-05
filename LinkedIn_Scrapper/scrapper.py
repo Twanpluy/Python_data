1de1be0a73222c2e4dd28fecd0ab132bdd2346b2
Twan Pluymaekers <twanpluy@gmail.com>
2023-02-05 21:19:33 +0100

    Working scrapper to json

--- LinkedIn_Scrapper/scrapper.py
+++ LinkedIn_Scrapper/scrapper.py
@@ -6,7 +6,6 @@ import time
 import os
 import datetime
 import json
-# TODO: TEST it out
 
 class LinkedIn_Scrapper:
     logging.basicConfig(level=logging.INFO)
@@ -14,10 +13,9 @@ class LinkedIn_Scrapper:
     URL = "https://www.linkedin.com/jobs/search/?keywords="
     FILEPATH = "datastore/"
 
-    def __init__(self,job,area):
+    def __init__(self, job, area):
         self.job = job
         self.area = area
-        
 
     # function Inputs for the job (job title) and area to search for (country))
     def input_search(self) -> str:
@@ -30,7 +28,7 @@ class LinkedIn_Scrapper:
     # Example: https://www.linkedin.com/jobs/search/?keywords=software%20engineer&location=United%20States
     # @param: create_url job (string), area (string) parameters for the functions
     def create_url(self) -> str:
-        """Create the url for the job search, input job (string) and area (string) """
+        """Create the url for the job search, input job (string) and area (string)"""
         job = self.job.replace(" ", "%20")
         area = self.area.replace(" ", "%20")
         Search_URL = f"{self.URL}{job}&location={area}"
@@ -42,81 +40,99 @@ class LinkedIn_Scrapper:
         logging.info(f"The following url will be used: \n {self.create_url()}")
         jobs = []
         response = requests.get(self.create_url())
-        soup = BeautifulSoup(response.content, "html.parser")
 
+        time.sleep(2.5)
+
+        logging.info(f"Response code: {response.status_code}")
+        soup = BeautifulSoup(response.content, "html.parser")
         # export html to file for testing
-        with open("datastore/linkedin.html", "w",encoding='utf-8') as f:
+        with open("datastore/linkedin.html", "w", encoding="utf-8") as f:
             f.write(str(soup))
             f.close()
 
-
         # Find all the job cards
+
+        Find_All_Job = soup.find_all("div", "base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card")
+# ul 
         # old search div class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card
-        Find_All_Job = soup.find_all("ul","scaffold-layout__list-container")
-        #write find all job in cards to file
-        with open("datastore/find_job.html", "w",encoding='utf-8') as f:
+        # write find all job in cards to file
+        with open("datastore/find_job.html", "w", encoding="utf-8") as f:
             f.write(str(Find_All_Job))
             f.close()
 
         # Loop through the job cards and extract the data
         for job in Find_All_Job:
-            if job.find("span", class_="job-search-card__salary") is not None:
-                job_id =  job.find("div", class_="data-job-id")
-            else:
-                job_id = "No job id"  
-            
+            # Get title of the job
             if job.find("h3", class_="base-search-card__title") is not None:
                 title = job.find("h3", class_="base-search-card__title").text.strip()
             else:
-                title = "No title"          
-            # title = job.find("h3", class_="base-search-card__title").text.strip()
+                title = "No title found"
 
             if job.find("h4", class_="base-search-card__subtitle") is not None:
                 company = job.find("h4", class_="base-search-card__subtitle").text.strip()
             else:
-                company = "No company"    
+                company = "No company found"    
 
-            # company = job.find("h4", class_="base-search-card__subtitle").text.strip()
             if job.find("span", class_="job-search-card__location") is not None:
-                location = job.find("span", class_="job-search-card__location").text.strip()
+                location = job.find("span", class_="job-search-card__location").text.strip()       
             else:
-                location = "No location"    
-            # location = job.find("span", class_="job-search-card__location").text.strip()
+                location = "No location found"
+
             if job.find("span", class_="job-search-card__salary") is not None:
                 salary = job.find("span", class_="job-search-card__salary").text.strip()
             else:
-                salary = "No salary"    
-            # salary = job.find("span", class_="job-search-card__salary").text.strip()
+                salary = "No salary found"                   
 
-        # Get job description for each job, need to open the job page tab
-            job_link = job.find("a", class_="base-card__full-link")["href"]
-            time.sleep(0.3)
-            job_response = requests.get(job_link)
-            time.sleep(0.3)
-            job_soup = BeautifulSoup(job_response.content, "html.parser")
-            job_soup.find("div", class_="show-more-less-html__markup show-more-less-html__markup--clamp-after-5") 
-            job_description = job_soup.find("div", class_="show-more-less-html__markup show-more-less-html__markup--clamp-after-5").text.strip()  
-            jobs.append({
-                "Job ID": job_id,
-                "Title": title,
-                "Company": company,
-                "Location": location,
-                "Salary": salary,
-                "Description": job_description
+            if job.find("span", class_="job-search-card__listdate") is not None:
+                job_id = job.find("span", class_="job-search-card__listdate").text.strip()
+            else:
+                job_id = "No job id found"
 
-            })
-            return jobs
+            if job.find("a", class_="base-card__full-link") is not None:
+                job_link = job.find("a", class_="base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]")["href"]
+            else:
+                job_link = "No link found"    
+
+            
+            # Get job description for each job, need to open the job page tab
+            logging.info(f"Opening job page: {job_link}")
+            job_details_response = requests.get(job_link)
+            time.sleep(2.5)
+            job_details_soup = BeautifulSoup(job_details_response.content, "html.parser")
+                    # write find all job in cards to file
+            with open("datastore/find_job_details.html", "w", encoding="utf-8") as f:
+                f.write(str(job_details_soup))
+                f.close()
+
+            if job_details_soup.find("div", class_="decorated-job-posting__details") is not None: 
+                job_description = job_details_soup.find("div", class_="decorated-job-posting__details").text.strip()
+            else:
+                job_description = "No description found"
+
+            jobs.append(
+                {
+                    "Job ID": job_id,
+                    "Title": title,
+                    "Company": company,
+                    "Location": location,
+                    "Salary": salary,
+                    "Description": job_description,
+                    "link": job_link,
+                }
+            )
+        return jobs
 
     # function to save the data to a csv file
     def save_to_json(self, jobs):
         """Save to json file"""
         date = datetime.datetime.now()
         complete_file_path = f"{self.FILEPATH}{self.job}_{self.area}_{date}"
-        complete_file_path = complete_file_path.replace(" ", "_").replace(":", "_").replace(".", "_").replace("-", "_")
+        complete_file_path = (
+            complete_file_path.replace(" ", "_")
+            .replace(":", "_")
+            .replace(".", "_")
+            .replace("-", "_")
+        )
         logging.info(f"{complete_file_path}")
         file = open(f"{complete_file_path}.json", "w")
         json.dump(jobs, file, indent=4)
-
-        
-
-
